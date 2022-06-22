9347eb4a0a5fb89179e9edd923416dbfadb02a03
Reza <nasimreza72>
2022-06-22 07:40:02 +0200

    added different language option on translation page

--- frontend/src/Translation.jsx
+++ frontend/src/Translation.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState, useRef } from "react";
 
 export default function Translation() {
   const [sentence, setSentence] = useState(null);
+  const [selectedLanguage, setSelectedLanguage] = useState("de");
   const [translatedSentence, setTranslatedSentence] = useState(null);
   const inputText = useRef();
 
@@ -10,6 +11,7 @@ export default function Translation() {
     
         let preTranSentence = {
           "preTranslatedSentence": sentence,
+          "preSelectedLanguage": selectedLanguage,
         };
     
         fetch(process.env.REACT_APP_URL+`/toTranslate`, {
@@ -34,6 +36,16 @@ export default function Translation() {
       </div>
       <div className="showTranslatedResult">
           {translatedSentence}
+          <select onChange={(e) => setSelectedLanguage(e.target.value)} name="selectLanguage" id="">
+          <option value="">Select a language</option>
+          <option value="en-US">English</option>
+          <option value="de">German</option>
+          <option value="it">Italian</option>
+          <option value="es">Spanish</option> 
+          <option value="pt-PT">Portuguese</option>
+          <option value="fr">French</option>
+          <option value="el">Greek</option>
+        </select>
       </div>
       <Button
         onClick = {submitHandler}
